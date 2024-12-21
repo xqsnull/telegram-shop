2f41c7b15b3c08b5990ee79c17385703a684cf5e
ArseniyKovpan <107571922+ArseniyKovpan@users.noreply.github.com>
2024-12-21 00:59:40 -0500

    Update cfg.py

--- main/cfg.py
+++ main/cfg.py
@@ -1,26 +1,13 @@
 # Модуль для параметров
 
 class Config:
-    token = "6873657186:AAHSLkemoa3a8Sf--M0wQGs26M5Kq-jX4rs"
-    payment_provider_token = '1744374395:TEST:046b2aff56cea75f295d'
-    admin_chat_id = 1638573890
-    admin_id = ('https://t.me/ggxanoqs')
+    token = "your-tocken"
+    payment_provider_token = 'your-payment-tocken'
+    admin_chat_id = 'admin-chat-id'
+    admin_id = ('admin-id')
     all_items = {'1000VB': 350,
                  '2000VB': 500,
                  '5000VB': 900,
                  '13500VB': 1300,
                  'Гремучие грёзы': 1400,
                  'Аватар': 800}
-
-# token = '6873657186:AAHSLkemoa3a8Sf--M0wQGs26M5Kq-jX4rs'
-# payment_provider_token = '1744374395:TEST:046b2aff56cea75f295d'
-# admin_chat_id = 1638573890
-
-# all_items = {
-#     '1000VB': 350,
-#     '2000VB': 500,
-#     '5000VB': 900,
-#     '13500VB': 1300,
-#     'Гремучие грёзы': 1400,
-#     'Аватар': 800
-# }         
